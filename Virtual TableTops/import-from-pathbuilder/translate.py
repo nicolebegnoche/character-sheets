c890e2922936df2e2a8c7d9fa1c87a1cf4aef9d7
Nicole Begnoche <29169568+nicolebegnoche@users.noreply.github.com>
2023-11-22 06:24:25 -0700

    Refactor

--- Virtual TableTops/import-from-pathbuilder/translate.py
+++ Virtual TableTops/import-from-pathbuilder/translate.py
@@ -4,28 +4,29 @@ import os
 input_file = os.path.join("exports", "pathbuilder_export.json")
 output_folder = "script_output"
 
+# this global variable does not change after being populated
+data = {}
+
 
 class Character:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, data):
+        self.name = data["name"]
         self.attribs = []
         self.abilities = []
 
     def __repr__(self):
-        padding = 15
+        padding = 25
         string = self.name + "\n"
-        for key in self.attribs:
+       
+        for key in self.attribs + self.abilities:
             string += str(key.get("name") + ":").ljust(padding)
-            string += str(key.get("current")) + "\n"
-
-        for key in self.abilities:
-            string += str(key.get("name") + ":").ljust(padding)
-            string += str(key.get("current")) + "\n"
-
+            string += str(key.get("current")) + "\n"  
         return string
 
-    def add_attribute(self, key, value):
-        self.attribs.append({"name": key, "current": value, "max": "", "id": ""})
+    def add(self, list_of_keys, target_list=None):
+        target = self.abilities if target_list == "abilities" else self.attribs
+        for key, value in list_of_keys:
+            target.append({"name": key, "current": value, "max": "", "id": ""})
 
     def export(self):
         export = {
@@ -41,39 +42,76 @@ class Character:
                 "abilities": self.abilities,
             },
         }
-
         write_to_json(export)
 
 
-def import_pathbuilder(data):
-    d = data["build"]
-    c = Character(d["name"])
+def main():
+    global data
+    data = read_json(input_file)["build"]
+    character = Character(data)
 
-    # combine ancestry with heritage without duplicating or omitting race
-    heritage = modify_heritage(d["ancestry"], d["heritage"])
+    # simple values that require no work to import  (name, deity, class, background, size, alignment, level, age, gender)
+    character.add(basics())
 
-    keys = [
-        ["age", d["age"]],
-        ["alignment", d["alignment"]],
-        ["ancestry_heritage", heritage],
-        ["background", d["background"]],
-        ["class", d["class"]],
-        ["deity", d["deity"]],
-        ["gender_pronouns", d["gender"]],
-        ["languages", ", ".join(d["languages"])],
-        ["level", d["level"]],
-        ["size", d["sizeName"]],
-    ]
+    # combine ancestry and heritage without duplicating or omitting ancestry
+    character.add(heritage())
 
+    character.add(ability_scores())
+    # character.add(hit_points())
+    # character.add(armor_class())
+    # character.add(saving_throws())
+    # character.add(skills())
+    # character.add(speed())
+    # character.add(weapon_proficiencies())
+    # character.add(class_dc())
+    # character.add(perception())
+
+    # write to file
+    character.export()
+    print(character)
+
+
+def basics():
+    """Import simple fields that require minimal conversion"""
+
+    matching_names = ["age", "alignment", "background", "class", "deity", "level"]
+    keys = [[x, data[x]] for x in matching_names]
+
+    keys.extend(
+        [
+            ["size", data["sizeName"]],
+            ["gender_pronouns", data["gender"]],
+            ["languages", ", ".join(data["languages"])],
+        ]
+    )
+    return keys
+
+def heritage():
+    """Combine ancestry and heritage observing the following rules:
+
+    Include the race name if absent but do not allow it to be duplicated
+        'Elf' and 'Ancient Elf' returns 'Ancient Elf'
+        'Dwarf' and 'Ancient-Blooded' returns 'Ancient-Blooded Dwarf'
+
+    For mixed ancestries, the heritage should not include 'human' but should include any other race
+        'Human' and 'Half-Elf' returns 'Half-Elf'
+        'Tiefling' and 'Half-Elf' returns 'Half-Elf Tiefling'
+    """
+    ancestry = data["ancestry"]
+    heritage = data["heritage"]
+    if ancestry not in heritage:
+        if not (ancestry == "Human" and "Half" in heritage):
+            heritage += " " + ancestry
+    return [["ancestry_heritage", heritage]]
+
+def ability_scores():
     # pathbuilder exports style: ['abilities']['str'] = 20
     # roll20 imports
     #       strength = 20
-    #       strength_score = "20"
+    #       strength_score = '20'
     #       strength_modifier = 5
-
     # this key is also present but doesn't appear to be necessary
-    #       strength_ability_check_modifier = ""
-
+    #       strength_ability_check_modifier = ''
     abilities = [
         "strength",
         "dexterity",
@@ -82,64 +120,43 @@ def import_pathbuilder(data):
         "wisdom",
         "charisma",
     ]
-
+    keys = []
     for ability in abilities:
         abbr = ability[:3]
-        score = d["abilities"][abbr]
+        score = data["abilities"][abbr]
         mod = (score - 10) // 2
 
         # I have triple-checked and yes, it really does require all three
         keys.append([ability, score])
         keys.append([ability + "_score", score])
         keys.append([ability + "_modifier", mod])
-
-    for k, v in keys:
-        c.add_attribute(k, v)
-
-    return c
+    return keys
 
 
-def modify_heritage(ancestry, heritage):
-    # Half-Elves and Half-Orc humans won't append "Human" but a Half-Orc Elf will
-    if "Half-" in heritage and ancestry == "Human":
-        return heritage
-
-    # "Skilled Heritage" becomes simply "Skilled"  (Human is added in next step)
-    if "Heritage" in heritage:
-        heritage = heritage.replace("Heritage", "").strip()
-
-    # "Ancient-Blooded" + "Dwarf" becomes "Ancient-Blooded Dwarf"
-    # but "Ancient Elf" + "Elf" does NOT become "Ancient Elf Elf" :)
-    if ancestry not in heritage:
-        heritage += " " + ancestry
-
-    return heritage
-
+def read_json(file):
+    if not os.path.exists(file):
+        print(f'File "{file}" does not exist.')
+        quit()
+    else:
+        try:
+            return json.load(open(input_file))
+        except Exception as e:
+            print("Exception occurred during data import:", e)
+            quit()
 
 def write_to_json(data):
     name = data["character"]["name"]
     filename = name.replace(" ", "") + ".json"
     output_file = os.path.join(output_folder, filename)
-
     try:
         if not os.path.exists(output_folder):
             os.makedirs(output_folder)
         json.dump(data, open(output_file, "w"), indent=4)
-
     except Exception as e:
         print("Exception occured while saving file: ", e)
         quit()
 
 
 if __name__ == "__main__":
-    try:
-        data = json.load(open(input_file))
-        character = import_pathbuilder(data)
-
-    except Exception as e:
-        print("Exception occurred during data import:", e)
-        quit()
-
-    character.export()
-
+    main()
     print("\nProgram Finished.")
